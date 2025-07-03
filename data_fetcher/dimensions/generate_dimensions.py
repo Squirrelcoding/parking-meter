0d7485e81d2567cf796f0074d3a32c34038e54a4
squirrelcoding <rosilescarlos28@gmail.com>
2025-07-03 11:09:53 -0500

    windshield generator

---
+++ data_fetcher/dimensions/generate_dimensions.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+from scipy.stats import multivariate_normal
+
+df = pd.read_csv("data/output.csv")
+
+mean = np.mean(df, axis=0)
+cov = np.cov(df, rowvar=0) #type: ignore
+
+y = multivariate_normal(mean=mean, cov=cov)
+print(y.rvs())
