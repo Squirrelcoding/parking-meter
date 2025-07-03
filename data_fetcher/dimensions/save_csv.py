03a16c16a7c43b6b9081c2ef3027005bdd6ae7bc
squirrelcoding <rosilescarlos28@gmail.com>
2025-07-03 01:17:58 -0500

    edf

--- data_fetcher/dimensions/save_csv.py
+++ data_fetcher/dimensions/save_csv.py
@@ -1,18 +1,14 @@
 import ast
 import csv
 
-# Load data from a text file
 with open("data/dimensions.txt", "r") as file:
     data = file.read()
 
-# Convert the string to an actual Python list of tuples
 points = ast.literal_eval(data)
 
-# Prepare the header and rows
 header = ['length', 'width']
 rows = [(point[0], point[1]) for point in points]
 
-# Write to a CSV file
 with open("output.csv", "w", newline='') as csvfile:
     writer = csv.writer(csvfile)
     writer.writerow(header)
