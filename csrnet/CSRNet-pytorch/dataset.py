cdd9e7102c13fc46c52bc5e83f303b2951dc283e
squirrelcoding <rosilescarlos28@gmail.com>
2025-07-15 09:50:11 -0500

    began updating code

--- csrnet/CSRNet-pytorch/dataset.py
+++ csrnet/CSRNet-pytorch/dataset.py
@@ -1,12 +1,7 @@
 import os
 import random
-import torch
-import numpy as np
 from torch.utils.data import Dataset
-from PIL import Image
 from image import *
-import torchvision.transforms.functional as F
-
 
 class listDataset(Dataset):
     def __init__(self, root, shape=None, shuffle=True, transform=None,  train=False, seen=0, batch_size=1, num_workers=4):
@@ -14,7 +9,7 @@ class listDataset(Dataset):
             root = root * 4
         random.shuffle(root)
 
-        self.nSamples = len(root)
+        self.n_samples = len(root)
         self.lines = root
         self.transform = transform
         self.train = train
@@ -24,7 +19,7 @@ class listDataset(Dataset):
         self.num_workers = num_workers
 
     def __len__(self):
-        return self.nSamples
+        return self.n_samples
 
     def __getitem__(self, index):
         assert index <= len(self), 'index range error'
