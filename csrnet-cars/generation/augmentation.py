a10da73e1ec27ffb71d56cc2575e6698d9454ca5
Squirrelcoding <rosilescarlos28@gmail.com>
2025-08-05 15:51:51 -0500

    moved files around and made train-test-val split

---
+++ csrnet-cars/generation/augmentation.py
@@ -0,0 +1,42 @@
+import random
+
+import torchvision.datasets as dset
+from torchvision.transforms import v2
+from torchvision.transforms import PILToTensor, ToPILImage
+import torch
+import pathlib
+
+# H, W = 416, 416
+
+
+dataset_path = pathlib.Path.home() / "Library/Mobile Documents/com~apple~CloudDocs/dataset"
+annotation_file = dataset_path / "annotations.json"
+
+
+def gauss_noise_tensor(img):
+    assert isinstance(img, torch.Tensor)
+    dtype = img.dtype
+    if not img.is_floating_point():
+        img = img.to(torch.float32)
+    
+    sigma = 10.0
+    
+    out = img + sigma * torch.randn_like(img)
+    
+    if out.dtype != dtype:
+        out = out.to(dtype)
+        
+    return out
+
+transforms = v2.Compose([
+    PILToTensor(),
+    gauss_noise_tensor,
+    ToPILImage(),
+    v2.RandomResize(125, 175),
+    v2.ColorJitter(brightness=0.3),
+])
+
+dataset = dset.CocoDetection(dataset_path, annFile=annotation_file, transform=transforms)
+print(len(dataset))
+
+img, target = random.choice(dataset)
---
+++ test.py
@@ -0,0 +1,19 @@
+import h5py
+import matplotlib.pyplot as plt
+from matplotlib import cm as CM
+
+# Open the .h5 file
+with h5py.File('IMG_167.h5', 'r') as f:
+    # Access the 'density' group
+    
+    # Assuming the dataset inside density is called 'data'
+    matrix = f['density'][:]  # load entire dataset into memory
+
+# Visualize with matplotlib
+plt.figure(figsize=(12, 8))
+plt.imshow(matrix, cmap=CM.jet)  # or 'plasma', 'inferno', 'magma', etc.
+plt.colorbar(label='Density')
+plt.title('Density Heatmap')
+plt.xlabel('X axis')
+plt.ylabel('Y axis')
+plt.show()
